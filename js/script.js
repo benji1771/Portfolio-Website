8f058e48182dc436ae0b87d7d44407d811d0e5c2
benji1771 <sarolbenjamin_08221999@yahoo.com>
2021-10-09 22:16:06 -0500

    box shadow on header when open

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const hamButton = document.querySelector('#hamButton');
 const hamburger = document.querySelector('.hamburger');
 const navi = document.querySelector('.Navi');
+const header = document.querySelector('header');
 hamButton.addEventListener('click', function(){
     console.log("hello");
     if(hamburger.classList.contains('open')){
         hamburger.classList.remove('open');
         navi.classList.remove('open');
+        header.classList.remove('open');
+        
     }else{
         hamburger.classList.add('open');
         navi.classList.add('open');
+        header.classList.add('open');
     }
 });
 
@@ -17,9 +21,11 @@ document.querySelectorAll('.Navi-Lists-Ref a').forEach(item => {
         if(hamburger.classList.contains('open')){
             hamburger.classList.remove('open');
             navi.classList.remove('open');
+            header.classList.remove('open');
         }else{
             hamburger.classList.add('open');
             navi.classList.add('open');
+            header.classList.add('open');
         }
     })
 });
